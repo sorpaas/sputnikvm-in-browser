8b3fbf39288919816a9cb966ef6de3b04f17ca30
Wei Tang <hi@that.world>
2017-11-03 21:07:29 +0800

    Add the VM fire sequence

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use block_core::{TransactionAction};
 use bigint::{Gas, H256, U256, Address};
 use rlp::Rlp;
 use hexutil::*;
+use std::rc::Rc;
+use std::str::FromStr;
 
 fn main() { }
 
@@ -17,5 +19,30 @@ fn main() { }
 
 #[no_mangle]
 pub fn run_example() -> i32 {
+    let header = HeaderParams {
+        beneficiary: Address::default(),
+        timestamp: 0x01,
+        number: U256::zero(),
+        difficulty: U256::zero(),
+        gas_limit: Gas::max_value(),
+    };
+
+    let context = Context {
+        address: Address::default(),
+        caller: Address::default(),
+        code: Rc::new(read_hex("0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff01600055").unwrap()),
+        data: Rc::new(Vec::new()),
+        gas_limit: Gas::from_str("0x0186a0").unwrap(),
+        gas_price: Gas::zero(),
+        origin: Address::default(),
+        value: U256::zero(),
+        apprent_value: U256::zero(),
+        is_system: false,
+    };
+
+    let mut vm = SeqContextVM::<MainnetEmbeddedPatch>::new(context, header.clone());
+    vm.commit_account(AccountCommitment::Nonexist(Address::default()));
+    vm.fire().unwrap();
+
     return 0;
 }
