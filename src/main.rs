edf89d036032f8e2885ea405f11578c7b79c7827
Wei Tang <hi@that.world>
2017-11-03 21:02:47 +0800

    Add sputnikvm dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-fn main() {
-    println!("Hello, world!");
-}
+extern crate sputnikvm;
+extern crate rlp;
+extern crate hexutil;
+extern crate bigint;
+extern crate block_core;
+
+use sputnikvm::{VM, HeaderParams, Context, SeqContextVM, MainnetEmbeddedPatch, RequireError, AccountCommitment, ValidTransaction, VMStatus, SeqTransactionVM};
+use block_core::{TransactionAction};
+use bigint::{Gas, H256, U256, Address};
+use rlp::Rlp;
+use hexutil::*;
+
+fn main() { }
 
 // Functions that you wish to access from Javascript
 // must be marked as no_mangle
